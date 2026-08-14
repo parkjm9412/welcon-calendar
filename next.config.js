c7a7351351843a01a8bf0024556eb43574a07d4a
Park JM <parkjm9412@gmail.com>
2026-08-14 09:21:31 +0900

    Fix: Use -p 3000 flag for Next.js start

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+  serverRuntimeConfig: {
+    port: process.env.PORT || 3000,
   },
 }
 
